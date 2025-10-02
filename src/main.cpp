62be13e7359d57086792d1ce1447cb89198db846
Vitaly Sandalov <vit00lya@yandex.ru>
2025-10-02 20:53:45 +0300

    Добавлен логотип и возможность переносить строки на дисплее на другую строку

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,70 @@
 #include <string>
 #include <string_view>
 #include <optional>
+#include <vector>
 #include "input_reader.h"
 #include "elevator_control.h"
 #include "json_reader.h"
 #include "xserial.hpp" 
 #include "magic_enum.hpp"
 
+using namespace std::literals;
+
 #if EXTERNAL_DISPLAY
+#include <codecvt>
+#include <locale>
+#include <algorithm>
 #include "wiringRP.h"
 #include "logo.h"
 #include "devices/ks0108.h"
 #include "devices/fonts/System5x7R.h"
-#endif
 
-using namespace std::literals;
+static const size_t MAX_LINES = 6;
+static const size_t HEIGHT_LINE = 10;
+static const size_t MAX_CHARS_PER_LINE = 21;
 
-void print_display_text(const std::wstring& str){
-  clearScreen();
-  writeString(0,5,str.c_str(),System5x7R);
-  syncBuffer();
+std::wstring Utf8ToWchar(const char* utf8_str) {
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    return converter.from_bytes(utf8_str);
 }
 
-void init_display(elevator_control::Settings& settings){
+void PrintLines(std::vector<std::wstring>& lines){
+  for (size_t i = 0; i < lines.size() && i < MAX_LINES; ++i){
+    writeString(0, 5 + HEIGHT_LINE * i, lines[i].c_str(), System5x7R);
+  }
+}
+
+std::vector<std::wstring> GetStrings(const std::wstring& str){
+
+  std::vector<std::wstring> result;
+  
+  if(str.size() < MAX_CHARS_PER_LINE){
+    result.push_back(str);
+  }
+  else{
+
+    size_t iter_count = str.size() /  MAX_CHARS_PER_LINE + 1;
+    size_t remaining_characters = str.size();
+    for(size_t i = 0; i < iter_count; ++i) {
+      size_t end_line = std::min(MAX_CHARS_PER_LINE,remaining_characters);
+      result.push_back(str.substr(MAX_CHARS_PER_LINE * i, end_line));
+      remaining_characters -= MAX_CHARS_PER_LINE - 1;
+      
+    }
+  }
+  return result;
+}
+
+void PrintDisplayText(const std::wstring& str){
+  
+  clearScreen();
+  std::vector<std::wstring> lines = GetStrings(str);
+  PrintLines(lines);
+  syncBuffer();
+  delay(5000);
+}
+
+void InitDisplay(elevator_control::Settings& settings){
     if(setupWiringRP(WRP_MODE_PHYS) < 0)
          exit(EXIT_FAILURE);
 
@@ -46,9 +88,11 @@ void init_display(elevator_control::Settings& settings){
     drawBitmap(logo,128,64);
      shiftBufferHorizontal(30);
      syncBuffer();
-     delay(10000);
+     delay(5000);
      clearScreen();
 }
+
+#endif
 
 int main(){
 
@@ -63,7 +107,7 @@ int main(){
   settings = ec.GetSettings();
 
   #if EXTERNAL_DISPLAY
-    init_display(settings);
+    InitDisplay(settings);
   #endif
   
   if(settings.scanner_enable){
@@ -93,10 +137,10 @@ int main(){
   while(true){
 
     #if EXTERNAL_DISPLAY
-      print_display_text(L"Готов");
+      PrintDisplayText(L"Введите штрихкод");
     #endif
-    std::cout << "Готов"s << "\n";
-    input_string = "";
+    std::cout << "Введите штрихкод"s << "\n";
+    input_string = ""s;
     if(settings.scanner_enable){
       input_string = scanner.getLine();
       scanner.flushRxAndTx();
@@ -108,10 +152,10 @@ int main(){
 
     try{
       barcode = ir.ParseLine(input_string);
-      if(barcode == "9999999999999"s){
+      if(barcode == "9999999999999"sv){
 	if(ec.EmptyBarcodesToSend()){
 	    #if EXTERNAL_DISPLAY
-	     print_display_text(L"Список пуст, нечего отправлять.");
+	     PrintDisplayText(L"Список пуст, нечего отправлять.");
             #endif
 	  std::cout << "Список пуст, нечего отправлять."s <<  "\n";
 	  continue;
@@ -126,7 +170,7 @@ int main(){
 	  }
 	catch(...){
 	    #if EXTERNAL_DISPLAY
-	     print_display_text(L"Ошибка при записи транспортного пакета в файл");
+	     PrintDisplayText(L"Ошибка при записи транспортного пакета в файл");
             #endif
 	   std::cerr << "Ошибка при записи транспортного пакета в файл"s << std::endl;
 	}
@@ -135,11 +179,18 @@ int main(){
 	std::optional<std::string_view> name_product = ec.GetNameProduct(barcode);
 
 	if(name_product.has_value()){
+	  #if EXTERNAL_DISPLAY
+	  std::string tmp_string = std::string(name_product.value());
+	  auto text_wstring = Utf8ToWchar(tmp_string.c_str());
+	  // std::wcout << text_wstring <<  std::endl;
+	   PrintDisplayText(text_wstring.c_str());
+	  // std::wstring st = L"Пл";
+          #endif
 	  std::cout << name_product.value() << std::endl;
 	}
 	else{
 	  #if EXTERNAL_DISPLAY
-	     print_display_text(L"Неопознанный штрихкод");
+	     PrintDisplayText(L"Неопознанный штрихкод");
           #endif
 	  std::cout << "Неопознанный штрихкод"s << std::endl;
 	}
