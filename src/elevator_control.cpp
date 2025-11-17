4c6a86daabf3e841e7fde84e14445c35cf70de3d
Vitaly Sandalov <vit00lya@yandex.ru>
2025-11-17 16:40:17 +0300

    Добавлены комментарии в  файле elevator_control

--- src/elevator_control.cpp
+++ src/elevator_control.cpp
@@ -2,6 +2,9 @@
 
 using namespace elevator_control;
 
+/// @brief Добавляет штрихкод номенклатуры в систему
+/// @param name_product Название номенклатуры
+/// @param barcode Штрихкод номенклатуры
 void ElevatorControl::AddBarcode(std::string& name_product, std::string& barcode){
        names_products_.push_back(std::move(name_product));
        barcodes_.push_back(std::move(barcode));
@@ -10,18 +13,25 @@ void ElevatorControl::AddBarcode(std::string& name_product, std::string& barcode
        barcode_map_[name_product_sv] = barcode_sv;
 }
 
+/// @brief Добавляет штрихкод в очередь для отправки
+/// @param barcode Штрихкод для отправки
 void ElevatorControl::AddBarcodeToSend(std::string& barcode){
      barcodes_to_send_.push_back(std::move(barcode));
 }
 
+/// @brief Возвращает идентификатор транспортного пакета
+/// @return Идентификатор транспортного пакета
 long ElevatorControl::GetTransportPacketId(){
   return transport_packet_id_;
 }
 
+/// @brief Увеличивает идентификатор транспортного пакета на 1
 void ElevatorControl::IncTrasportPacketId(){
-  ++transport_packet_id_; 
+  ++transport_packet_id_;
 }
 
+/// @brief Проверяет, пуста ли очередь штрихкодов для отправки
+/// @return true, если очередь пуста, иначе false
 bool ElevatorControl::EmptyBarcodesToSend(){
   return barcodes_to_send_.empty();
 }
@@ -29,6 +39,7 @@ bool ElevatorControl::EmptyBarcodesToSend(){
 std::vector<std::string> ElevatorControl::GetBarcodesToSend(){
   return barcodes_to_send_;
 }
+
 
 void ElevatorControl::SaveSettings(Settings& settings){
   settings_ = std::move(settings);
@@ -38,6 +49,9 @@ Settings ElevatorControl::GetSettings(){
   return settings_;
 }
 
+/// @brief Возвращает название продукта по штрихкоду
+/// @param barcode Штрихкод продукта
+/// @return Название продукта, если штрихкод найден, иначе пустой optional
 std::optional<std::string_view> ElevatorControl::GetNameProduct(std::string_view barcode){
 
   if (barcode.empty()) {
@@ -51,6 +65,7 @@ std::optional<std::string_view> ElevatorControl::GetNameProduct(std::string_view
   
   return {};
 }
+
 
 
 
