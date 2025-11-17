7829c2f23ca6183fdd4f6f9444fc6f26e7f0147d
Vitaly Sandalov <vit00lya@yandex.ru>
2025-11-17 22:00:10 +0300

    Добавлены комментарии в файл parseline

--- src/input_reader.cpp
+++ src/input_reader.cpp
@@ -6,6 +6,17 @@ using namespace std::literals;
 namespace input_reader {
 
   std::string InputReader::ParseLine(std::string_view line) {
+  /**
+   * @brief Парсит строку штрихкода и возвращает корректный формат
+   *
+   * Функция обрабатывает строки штрихкодов следующих форматов:
+   * - Начинающиеся с '2' или '9': возвращаются первые 13 символов
+   * - Начинающиеся с '0': возвращаются символы с 2-го по 14-й (индексы 2-14)
+   *
+   * @param line Строка штрихкода для парсинга
+   * @return std::string Отформатированная строка штрихкода
+   * @throws std::invalid_argument Если строка не является корректным штрихкодом
+   */
   if (line.empty() || line.length() < 13){
      throw std::invalid_argument("Входящая строка не является штрихкодом"s);
   }
